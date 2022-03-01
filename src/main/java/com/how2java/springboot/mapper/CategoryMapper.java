8ecab878ba265c7182f6162eb6445307129937ca
Ian-fg <wangfei3415@sina.com>
2022-03-01 09:12:27 +0800

    ianfg-springboot

---
+++ src/main/java/com/how2java/springboot/mapper/CategoryMapper.java
@@ -0,0 +1,13 @@
+package com.how2java.springboot.mapper;
+
+import com.how2java.springboot.pojo.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+
+    List<Category> findAll();
+}
